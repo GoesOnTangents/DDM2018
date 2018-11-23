1bb1ffa0e6ee8db8f5d39e59e61b04fd33f056cb
Alexander Löser <alexander.loeser@student.hpi.uni-potsdam.de>
2018-11-23 20:38:16 +0100

    implement linear combination (working, but needs early exit for a huge speedup)

--- src/main/scala/com/example/Master.scala
+++ src/main/scala/com/example/Master.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import com.example.MasterActor.Read
-import com.example.SlaveActor.CrackPasswordsInRange
+import com.example.SlaveActor.{CrackPasswordsInRange, SolveLinearCombinationInRange}
 import com.typesafe.config.ConfigFactory
 
 import scala.util.control.Breaks.{break, breakable}
@@ -16,6 +16,7 @@ object MasterActor {
   case object SlaveSubscription
   case object PasswordFound
   case object SolveLinearCombination
+  case class LinearCombinationFound(combination: Long)
   case class PasswordFound(index: Int, password: Int)
 }
 
@@ -37,10 +38,11 @@ class MasterActor extends Actor {
 
   //counter variables
   var num_cracked_passwords = 0
+  var linear_combination_found = false
 
   def read(filename: String): Unit = {
     val file_contents =
-      scala.io.Source.fromFile(filename).getLines().drop(1) //TODO dont hardcode filename
+      scala.io.Source.fromFile(filename).getLines().drop(1)
     breakable {
       for (line <- file_contents) {
         if (line == "") break
@@ -67,7 +69,9 @@ class MasterActor extends Actor {
     case PasswordFound(id, pw) =>
       this.store_password(id,pw)
     case SolveLinearCombination =>
-      this.solve_linear_combination()
+      this.delegate_linear_combination()
+    case LinearCombinationFound(combination: Long) =>
+      this.store_linear_combination(combination)
     case Read(filename) =>
       this.read(filename)
     case msg: Any => throw new RuntimeException("unknown message type " + msg);
@@ -75,7 +79,7 @@ class MasterActor extends Actor {
   }
 
   def delegate_password_cracking(): Unit = {
-    println("Delegating Passwords to Crack.")
+    println("Delegating Passwords to crack.")
     val ranges = PasswordWorker.range_split(100000, 999999, slaves.length)
     for (i <- slaves.indices) {
       slaves(i) ! CrackPasswordsInRange(hashes, ranges(i)._1, ranges(i)._2)
@@ -83,7 +87,7 @@ class MasterActor extends Actor {
   }
 
   def subscribe_slaves(): Unit = {
-    this.slaves = this.slaves :+ this.sender()
+    slaves = slaves :+ sender()
     println(s"Current master's slaves:\n ${slaves.deep.mkString("\n")}")
     if (slaves.length == expectedSlaveAmount) {
       self ! CrackPasswords
@@ -101,9 +105,47 @@ class MasterActor extends Actor {
     }
   }
 
-  def solve_linear_combination(): Unit = {
+  def delegate_linear_combination(): Unit = {
+    println("Delegating linear combinations to test.")
 
+    //we assume that the sum is always an even number and we thus can divide by 2 - otherwise a partition cannot exist anyway
+    val target_sum: Long = cracked_passwords.sum / 2
+    println(s"total sum is ${2*target_sum}, so we look for $target_sum as sum")
+    val ranges = LinearCombinationWorker.range_split(0L, (1L << cracked_passwords.length) - 1, slaves.length)
+    for (i <- slaves.indices) {
+      slaves(i) ! SolveLinearCombinationInRange(cracked_passwords, ranges(i)._1, ranges(i)._2, target_sum)
+    }
   }
+
+  def store_linear_combination(combination: Long): Unit = {
+    if (linear_combination_found) {
+      var sum: Long = 0
+      var alternateSum: Long = 0
+      for (index <- linear_combination.indices) {
+        if ((combination & (1L << index)) > 0) {
+          sum += cracked_passwords(index)
+        } else {
+          alternateSum += cracked_passwords(index)
+        }
+      }
+      println(s"Already found a linear combination, so ignoring the new combination $combination, however sum is $sum and alternateSum is $alternateSum")
+      return
+    }
+
+    var sum: Long = 0
+    for (index <- linear_combination.indices) {
+      linear_combination(index) = (combination & (1L << index)) > 0
+      if (linear_combination(index)) {
+        sum += cracked_passwords(index)
+      }
+    }
+    println(s"sum is $sum")
+    linear_combination_found = true
+
+    //TODO trigger next step
+    println("TODO: trigger gene subsequence tasks")
+  }
+
 
   def findLcsPartners(): Unit = {
     //TODO: Step 1: Give slaves genes
--- src/main/scala/com/example/Slave.scala
+++ src/main/scala/com/example/Slave.scala
@@ -3,10 +3,75 @@ package com.example
 import java.io.File
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import com.example.MasterActor.{PasswordFound, SlaveSubscription}
-import com.example.PasswordWorker.Start
-import com.example.SlaveActor.{CrackPasswordsInRange, Subscribe}
+import com.example.MasterActor.{LinearCombinationFound, PasswordFound, SlaveSubscription}
+import com.example.SlaveActor.{CrackPasswordsInRange, SolveLinearCombinationInRange, Subscribe}
 import com.typesafe.config.ConfigFactory
+
+import scala.util.control.Breaks.{break, breakable}
+
+object LinearCombinationWorker {
+  final val props: Props = Props(new LinearCombinationWorker())
+  final case class Start(passwords: Array[Int], i: Long, j: Long, target_sum: Long)
+
+  //all values inclusive
+  //TODO: this is a (almost-)duplicate of PasswordWorker.range_split
+  //TODO: if we got too much time, we could figure out how generics work in scala and reduce redundancy
+  def range_split(min: Long, max: Long, num_batches: Int): Vector[Tuple2[Long,Long]] = {
+    var result = Vector[Tuple2[Long,Long]]()
+
+    val total_range = max - min + 1L
+
+    val range_per_batch : Long = total_range / num_batches
+    val additional_stuff_for_first_batch : Long = total_range % num_batches
+    var i : Long = min
+    var j : Long = min + range_per_batch - 1L + additional_stuff_for_first_batch
+    for (_ <- 1 to num_batches) {
+      result = result :+ (i, j)
+      i = j + 1
+      j += range_per_batch
+    }
+
+    result
+  }
+}
+
+class LinearCombinationWorker extends Actor {
+  import LinearCombinationWorker._
+
+  override def receive: Receive = {
+    case Start(passwords, i, j, sum) =>
+      this.solve_linear_combination(passwords, i, j, sum)
+  }
+
+  def solve_linear_combination(passwords: Array[Int], min: Long, max: Long, target_sum: Long): Unit = {
+    val masterActorAddress: String = "akka.tcp://MasterSystem@127.0.0.1:42000/user/MasterActor"
+    val masterActor = context.actorSelection(masterActorAddress)
+    println(s"$this has started to go through passwords from $min to $max")
+
+    var combination: Long = min
+    while (combination <= max) {
+      var sum = 0;
+      breakable {
+        for (index <- passwords.indices) {
+          if ((combination & (1L << index)) > 0) {
+            sum += passwords(index)
+            if (sum > target_sum) {
+              break
+            }
+          }
+        }
+      }
+      if (sum == target_sum) {
+        //Combination found
+        println("found a combination: " + combination)
+        masterActor ! LinearCombinationFound(combination)
+      }
+      combination += 1
+    }
+
+    context.stop(self)
+  }
+}
 
 object PasswordWorker {
   final val props: Props = Props(new PasswordWorker())
@@ -65,22 +130,28 @@ class PasswordWorker() extends Actor {
   override def receive: Receive = {
     case Start(passwords,i,j) =>
       this.crack_passwords_in_range(passwords,i,j)
-    }
+  }
 }
 
 object SlaveActor {
   final case class CrackPasswordsInRange(passwords: Array[String], i: Int, j: Int)
+  final case class SolveLinearCombinationInRange(passwords: Array[Int], min: Long, max: Long, target_sum: Long)
   final val props: Props = Props(new SlaveActor())
   final case class Subscribe(addr: String)
+
+  final val num_local_workers = 2 //TODO: dont hardcode
 }
 
 class SlaveActor extends Actor {
+  import SlaveActor._
 
   override def receive: Receive = {
-    case CrackPasswordsInRange(passwords,i,j) =>
-      this.start_password_workers(passwords, i, j)
     case Subscribe(addr) =>
       this.subscribe(addr)
+    case CrackPasswordsInRange(passwords, i, j) =>
+      this.start_password_workers(passwords, i, j)
+    case SolveLinearCombinationInRange(passwords, i, j, sum) =>
+      this.start_linear_combination_workers(passwords, i, j, sum)
   }
 
   def subscribe(addr: String) : Unit = {
@@ -89,13 +160,24 @@ class SlaveActor extends Actor {
   }
 
   def start_password_workers(passwords: Array[String], min: Int, max: Int): Unit = {
-    val num_local_workers = 2 //TODO dont hardcode
+    import PasswordWorker.Start
+
+
     val ranges = PasswordWorker.range_split(min, max, num_local_workers)
-    for (i <- 0 until num_local_workers) {
+    for (i <- ranges.indices) {
       val worker = context.actorOf(PasswordWorker.props, "PasswordCrackerWorker" + i)
       worker ! Start(passwords, ranges(i)._1,ranges(i)._2)
     }
+  }
 
+  def start_linear_combination_workers(passwords: Array[Int], min: Long, max: Long, target_sum: Long): Unit = {
+    import LinearCombinationWorker.Start
+
+    val ranges = LinearCombinationWorker.range_split(min, max, num_local_workers)
+    for (i <- ranges.indices) {
+      val worker = context.actorOf(LinearCombinationWorker.props, "LinearCombinationWorker" + i)
+      worker ! Start(passwords, ranges(i)._1,ranges(i)._2, target_sum)
+    }
   }
 }
 
