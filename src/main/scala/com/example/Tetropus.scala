acb7318b3d2129e0833b2c440852ac15f7fc49dd
Kim Borchart <Kim-Pascal.Borchart@student.hpi.de>
2018-11-23 14:18:22 +0100

    add Tetropus file as main (name not final)

---
+++ src/main/scala/com/example/Tetropus.scala
@@ -0,0 +1,12 @@
+package com.example
+
+import java.io.File
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+
+object Tetropus extends App {
+  if (args.length == 0) {
+    println("dude, you didn't give me any parameters")
+  } else if ()
+
+}
